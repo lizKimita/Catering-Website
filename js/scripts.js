b05b49aec6e04d4e464025294b47826c31383816
lizKimita <kimita.wanjiku@gmail.com>
2019-02-19 15:06:19 +0300

    Adds button that changes table background

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
   $("button#light-dark").click(function(){
     $(".fade1").fadeToggle();
   });
+  $("button#background-change").click(function(){
+  
+    $("table").addClass("black-background");
+  });
+
 });
